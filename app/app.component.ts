613d2585d0be0ec2ec5f8c3adbc1e301c3e98b06
Eric Krause <krause197@gmail.com>
2016-10-21 15:31:41 -0700

    Inital code into TypeScript files

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'meal-tracker',
+  template: `
+    <div class="container">
+      <div class="jumbotron text-center">
+        <h1>Meal Tracker</h1>
+      </div>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
